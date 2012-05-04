ee65feb279e10a53f35430b09eb406c6e8816247
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-05-04 16:45:05 +0200

    Doesn't look like this serves any real purpose

--- public_html/nojavascript.comment.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!isset($_ANONNEWS)) { die(); }  /* Protect from direct requests */
-/* This module handles the posting of replies to comments for users that do not have Javascript enabled. */
-
-
-?>
