93244cab30f5f1d2188f15e9631275abb83d76ea
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-10-17 17:27:41 +0200

    Basic page design

---
+++ public_html/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+	<head>
+		<title>AnonNews.org</title>
+		<link href='http://fonts.googleapis.com/css?family=Istok+Web:400,700,400italic' rel='stylesheet' type='text/css'>
+		<link href="static/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="page-header">
+			<h1><strong>AnonNews.org</strong> Everything Anonymous</h1>
+			<nav>
+				<a href="/popular" class="active">Most popular</a>
+				<a href="/recent">Most recent</a>
+				<a href="/trash">Trashbin</a>
+				<a href="/forum" class="section">Forums</a>
+				<a href="/faq" class="section">FAQ</a>
+				<a href="/donate" class="section" id="button_donate">Donate</a>
+				<a href="/submit" class="submit">Submit a press release or article</a>
+				<div class="clear-left"></div>
+			</nav>
+		</div>
+		
+		<div class="license">
+			<p>Header background image by <a href="http://www.flickr.com/photos/seier/4338268272/">seier+seier</a> used under a Creative Commons Attribution license.</p>
+			<p>All content on this website, including design and written material, is automatically licensed under a Creative Commons Attribution license. You are free to redistribute and/or remix it, but you have to credit the author, or, if the author is unknown ("Anonymous"), place a backlink to the corresponding page on AnonNews and attribute it to "Anonymous".</p>
+		</div>
+	</body>
+</html>
