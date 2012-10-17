3af01bbd4b338458e660c50e03d56fa23d024a70
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-10-17 03:31:02 +0200

    Remove all the old code

--- public_html/static/radio.static.php
+++
@@ -1,4 +0,0 @@
-<?php if(!isset($_ANONNEWS)) { die(); }  /* Protect from direct requests */ ?>
-<h2>AnonNews Radio has been discontinued</h2>
-<p>AnonNews Radio no longer exists. Although this may change in the future, there are no direct plans to set up AnonNews Radio again.</p>
-<p>If you are looking for new music, a good place to look would be <a href="http://www.jamendo.com/">Jamendo</a> - a website with thousands of freely shareable (Creative Commons-licensed) tracks.</p>
